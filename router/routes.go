8ecb57bad9158d034a70a657214e3dc83c0b8804
Fernando <dev25@redepos.com.br>
2025-02-01 22:31:11 -0300

    feath update handler structure to separete package

--- router/routes.go
+++ router/routes.go
@@ -1,41 +1,17 @@
 package router
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"github.com/newonne/gopportunities/handler"
 )
 
 func InitializeRoutes(router *gin.Engine) {
 	v1 := router.Group("/api/v1") // Fixed variable assignment
 
-	v1.GET("/opening", func(c *gin.Context) { // Fixed function declaration
-		c.JSON(http.StatusOK, gin.H{ // Fixed JSON response
-			"msg": "GET Opening",
-		})
-	})
+	v1.GET("/opening", handler.ShowOpeningHandler)
+	v1.POST("/opening", handler.CreateOpeningHandler)
+	v1.DELETE("/opening", handler.DeleteOpeningHandler)
+	v1.PUT("/opening", handler.UpdateOpeningHandler)
+	v1.GET("/openings", handler.ListOpeningsHandler)
 
-	v1.POST("/opening", func(c *gin.Context) { // Fixed function declaration
-		c.JSON(http.StatusOK, gin.H{ // Fixed JSON response
-			"msg": "POST t Opening",
-		})
-	})
-
-	v1.DELETE("/opening", func(c *gin.Context) { // Fixed function declaration
-		c.JSON(http.StatusOK, gin.H{ // Fixed JSON response
-			"msg": "DELETE Opening",
-		})
-	})
-
-	v1.PUT("/opening", func(c *gin.Context) { // Fixed function declaration
-		c.JSON(http.StatusOK, gin.H{ // Fixed JSON response
-			"msg": "UPDATE Opening",
-		})
-	})
-
-	v1.GET("/openings", func(c *gin.Context) { // Fixed function declaration
-		c.JSON(http.StatusOK, gin.H{ // Fixed JSON response
-			"msg": "GET Openings",
-		})
-	})
 }
